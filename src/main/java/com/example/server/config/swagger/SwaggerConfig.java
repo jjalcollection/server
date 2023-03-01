f414b9aae3ee746cea6196cdb07876540dc08933
chujonghyun <code201220@gmail.com>
2023-03-01 15:47:52 +0900

    feat : 스프링 기본 세팅

---
+++ src/main/java/com/example/server/config/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.server.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("JJalBang Project API")
+                        .description("짤빵생성기 프로젝트 API 명세서입니다.")
+                        .version("v0.0.1")
+                );
+    }
+}
