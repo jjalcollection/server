75fd1363ab2b2febdff9a708c8a5f0d581e7c056
bell <code201220@gmail.com>
2023-03-31 18:25:42 +0900

    feat : swagger 요청 url 세팅

--- src/main/java/com/example/server/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/server/config/swagger/SwaggerConfig.java
@@ -1,10 +1,16 @@
 package com.example.server.config.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = { @Server(url = "https://jjal.xyz"),
+        @Server(url = "http://localhost:8080/")})
+@Configuration
 public class SwaggerConfig {
 
     @Bean
